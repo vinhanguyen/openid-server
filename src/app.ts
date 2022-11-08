108407d9130e17d660a05c9921d3a01df8145f6d
vinhanguyen <vinh.a.nguyen@gmail.com>
2022-11-08 14:42:51 -0500

    Initial api with jwt auth

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { router } from './router';
+import cors from 'cors';
+import passport from 'passport';
+import { buildStrategy } from './strategy';
+
+const issuer = process.env.ISSUER;
+const client_id = process.env.CLIENT_ID;
+const jwks_uri = process.env.JWKS_URI;
+
+if (!(issuer && client_id && jwks_uri)) {
+  throw 'Missing ISSUER, CLIENT_ID, JWKS_URI env vars';
+}
+
+passport.use(buildStrategy(issuer, client_id, jwks_uri));
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.use('/', passport.authenticate('custom', { session: false }), router);
+
+app.listen(port, () => {
+  console.log(`openid-server listening at http://localhost:${port}`);
+});
---
+++ src/strategy.ts
@@ -0,0 +1,55 @@
+import { createRemoteJWKSet, jwtVerify } from "jose";
+import { Strategy } from "passport-custom";
+import { createFederatedCredential, createUser, getFederatedCredential, getUser } from "./dao";
+
+export function buildStrategy(issuer: string, audience: string, jwks_uri: string) {
+  const JWKS = createRemoteJWKSet(new URL(jwks_uri));
+
+  return new Strategy(
+    async function(req, done) {
+      // get id token from header
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return done(null, false);
+      }
+
+      const match = authHeader.match(/Bearer (.+)/);
+      if (!match) {
+        return done(null, false);
+      }
+  
+      const jwt = match[1];
+      try { // verify token
+        const { payload, protectedHeader } = await jwtVerify(jwt, JWKS, {
+          issuer,
+          audience,
+        });
+
+        // get user
+        try {
+          const {iss = null, sub = null, name = null} = payload;
+  
+          if (!(iss && sub && name)) {
+            return done('Missing payload');
+          }
+  
+          const cred: any = await getFederatedCredential(iss, sub);
+
+          if (!cred) {
+            const user: any = await createUser(<string>name);
+            await createFederatedCredential(user.id, iss, sub);
+            return done(null, user);
+          } else {
+            const user = await getUser(cred.user_id);
+            if (!user) { return done(null, false); }
+            return done(null, user);
+          }
+        } catch (e) { // db error
+          return done(e);
+        }
+      } catch (e) { // verify failed
+        return done(null, false);
+      }
+    }
+  );
+}
