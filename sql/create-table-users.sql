ab8b99d7bf98ddebc30694339d48a48a493bf56d
vinhanguyen <vinh.a.nguyen@gmail.com>
2022-11-30 07:43:44 -0500

    Change varchars to 255

--- sql/create-table-users.sql
+++ sql/create-table-users.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(100)
+  email VARCHAR(255)
 );
 
 CREATE TABLE federated_credentials (
   user_id INT NOT NULL REFERENCES users(id),
-  provider VARCHAR(100) NOT NULL,
-  subject VARCHAR(100) NOT NULL,
+  provider VARCHAR(255) NOT NULL,
+  subject VARCHAR(255) NOT NULL,
   PRIMARY KEY(provider, subject)
 );
